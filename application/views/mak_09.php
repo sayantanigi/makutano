6dce685b790309aa12d68cce050180f2b4150dde
sayantanigi <sayantan@goigi.in>
2024-02-19 18:19:00 +0530

    update code

--- application/views/mak_09.php
+++ application/views/mak_09.php
@@ -1,6 +1,6 @@
 <main>
     <section class="po-rel-z1 pt-100 pb-145">
-        <img src="assets/img/rdc_Makutano9_Abidjan_English_pages-to-jpg-0001-2048x1152.webp" class="img-fluid">
+        <img src="<?= base_url()?>assets/img/rdc_Makutano9_Abidjan_English_pages-to-jpg-0001-2048x1152.webp" class="img-fluid">
         <div class="container">
             <?php if(!empty($mak_zeronine)) { 
             foreach ($mak_zeronine as $val) { ?>
@@ -9,7 +9,7 @@
         </div>
         <div>
             
-            <img src="assets/img/mak-09-14.webp" class="img-fluid">
+            <img src="<?= base_url()?>assets/img/mak-09-14.webp" class="img-fluid">
         </div>
     </section>
 </main>
